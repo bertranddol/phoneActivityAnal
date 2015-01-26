6c046a1270ffcc0de4d435f4ea1aea7b6b556e7d
Bertrand Dolimier <bdolimier@millennialmedia.com>
2015-01-25 19:17:52 -0500

    activity and ubject

--- run_analysis.R
+++ run_analysis.R
@@ -13,7 +13,6 @@ listset[[2]] = read.table(train_human_activity_ds)
 
 mergedata = merge( listset[[1]] , listset[[2]] , all=TRUE )
 
-
 #2 Extracts only the measurements on the mean and standard deviation for each measurement. 
 featuredata <- read.table(feature_name)
 colNb = 0 
@@ -21,7 +20,6 @@ newcolumns <- ""
 
 # Looping on all the features / column name
 for (ii in 1:length( featuredata[[1]]  ) ) {
-  #setnames( mergedata, paste("V", ii , sep="") , as.character( featuredata[[ii,2]] ) )
   if ( grepl( "mean", as.character( featuredata[[ii,2]] ) ) == TRUE ) { 
     colNb = colNb + 1 
     newcolumns[colNb] <- as.character( featuredata[[ii,2]] )
@@ -33,7 +31,10 @@ for (ii in 1:length( featuredata[[1]]  ) ) {
 }
 
 rowNb <- nrow(mergedata)
-m <- as.data.frame(matrix(0, ncol = colNb , nrow = rowNb ) )
+m <- as.data.frame(matrix(0, ncol = colNb+2 , nrow = rowNb ) )
+newcolumns[colNb+1] <- "subject"
+newcolumns[colNb+2] <- "activity"
+
 m <- setnames( m , newcolumns )
 colNb = 0  
 for (ii in 1:length( featuredata[[1]]  ) ) {
@@ -56,15 +57,25 @@ for (ii in 1:length( featuredata[[1]]  ) ) {
 
 #5 From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-# Add the 
-for (ii in 1:length( featuredata[[1]]  ) ) {
-  if ( grepl( "mean", as.character( featuredata[[ii,2]] ) ) == TRUE ) { 
-    colNb = colNb + 1 
-    m[,colNb] <- mergedata[,ii]
-  }
-  if ( grepl( "std", as.character( featuredata[[ii,2]] ) ) == TRUE ) {
-    colNb = colNb + 1
-    m[,colNb] <- mergedata[,ii]
-  }
-}
+# Adding the subject 
+train_subject_ds <- "/Users/bdolimier/developer/perso/classDS/data/UCI_HAR_Dataset/train/subject_train.txt"
+test_subject_ds <- "/Users/bdolimier/developer/perso/classDS/data/UCI_HAR_Dataset/test/subject_test.txt"
+listset <- list()
+listset[[1]] = read.table(train_subject_ds)
+listset[[2]] = read.table(test_subject_ds)
 
+allsubject <- as.data.frame(matrix(0, ncol = 1 , nrow = nrow(m) ) )
+allsubject = merge( listset[[1]] , listset[[2]] , all=TRUE )
+colNb=colNb+1
+m[,colNb] <- allsubject[,1]
+
+# Adding activities
+train_y_ds <- "/Users/bdolimier/developer/perso/classDS/data/UCI_HAR_Dataset/train/y_train.txt"
+test_y_ds <- "/Users/bdolimier/developer/perso/classDS/data/UCI_HAR_Dataset/test/y_test.txt"
+tr_y = read.table(train_y_ds)
+te_y = read.table(test_y_ds)
+
+ally <- as.data.frame(matrix(0, ncol = 1 , nrow = nrow(m) ) )
+ally = merge( tr_y , te_y )
+colNb=colNb+1
+m[,colNb] <- ally[,1]
